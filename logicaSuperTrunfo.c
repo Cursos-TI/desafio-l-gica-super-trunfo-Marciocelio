cfb0ea3610d1ae1dc1fad7cbe7d6771b291e3d52
Marciocelio <francisco.ss.nasc@gmail.com>
2025-04-06 19:15:15 +0000

    Comparando Cartas do Super TrunfoNV.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+
  //BASE DA CARTA 1
 int carta1;
 char estado1 [40];
@@ -10,7 +11,10 @@ int populacao1;
 float area1;
 float PIB1;
 int Turistico1;
-
+float densidade1;
+float PIBpercapta1;
+float ValorTotal1;
+float Superpoder1;
 
 //BASE DA CARTA 2
 int carta2;
@@ -21,6 +25,10 @@ int populacao2;
 float area2;
 float PIB2;
 int Turistico2;
+float densidade2;
+float PIBpercapta2;
+float ValorTotal2;
+float Superpoder2;
 
 
 //PESQUISA DA CARTA A
@@ -61,6 +69,22 @@ printf("PIB: %.2f bilhões de reais \n", PIB1);
 printf("Número de Pontos Turísticos: %d \n", Turistico1);
 printf("\n");
 
+densidade1 = (populacao1 / area1);
+PIBpercapta1 = (PIB1/populacao1);
+
+printf("A Densisdade Populacional da carta 1 é: %.2f\n", densidade1);
+printf("O PIB per Capita da carta 1 é: %.2f\n", PIBpercapta1); 
+
+printf("\n");
+
+//BASE DE CALCULO DENSIDADE P. E PIB PER CAPITA
+float signedNumber1 = densidade1;
+float unsignedNumber1 = PIBpercapta1;
+
+printf("\n");
+printf("O valor total entre Densidade Populacional e o PIB per Capita é: %.2f\n", signedNumber1 + unsignedNumber1);
+printf("\n");
+
 
 //PESQUISA DA CARTA 2
 printf ("> REQUISITOS CARTA: 02\n");
@@ -99,6 +123,23 @@ printf("PIB: %.2f bilhões de reais \n", PIB2);
 printf("Número de Pontos Turísticos: %d \n", Turistico2);
 printf("\n");
 
+densidade2 = (populacao2 / area2);
+PIBpercapta2 = (PIB2/populacao2);
+
+printf("A Densisdade Populacional da carta 2 é: %.2f\n", densidade2);
+printf("O PIB per Capita da carta 2 é: %.2f\n", PIBpercapta2); 
+
+
+//BASE DE CALCULO DENSIDADE P. E PIB PER CAPITA
+float signedNumber2 = densidade2;
+float unsignedNumber2 = PIBpercapta2;
+
+printf("\n");
+printf("O valor total entre Densidade Populacional e o PIB per Capita é: %.2f\n", signedNumber2 + unsignedNumber2);
+printf("\n");
+
+
+
 printf("COMPARAÇÃO DAS CARTAS 1 E 2:\n");
 
 if (populacao1>populacao2) {
@@ -124,14 +165,31 @@ printf ("A Cidade 1 tem maior numero de ponto turístico.\n"); }
 else
 { printf("A Cidade 2 tem maior numero de ponto turístico.\n");}
 
+printf("\n");
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+printf ("CIDADE VENCEDORA!\n");
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+int cidadeVencedora;
 
+if (populacao1 > populacao2){
+    printf("A Cidade 1 População: Venceu!\n", cidadeVencedora); }
+else
+{ printf ("A Cidade 2 População: Venceu!\n", cidadeVencedora);}
+
+if (area1 > area2){
+    printf("A Cidade 1 Área: Venceu!\n", cidadeVencedora); }
+else
+{ printf ("A Cidade 2 Área: Venceu!\n", cidadeVencedora);}
+
+if (PIB1 > PIB2){
+    printf("A Cidade 1 PIB: Venceu!\n", cidadeVencedora); }
+else
+{ printf ("A Cidade 2 PIB: Venceu!\n", cidadeVencedora);}
+
+if (Turistico1 > Turistico2){
+    printf("A Cidade 1 N° de ponto turísticos: Venceu!\n", cidadeVencedora); }
+else
+{ printf ("A Cidade 2 Venceu!\n", cidadeVencedora);}
 
 
     return 0;
